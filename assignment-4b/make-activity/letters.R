c5741d7660ea2672c76293bd39b8a982b1b3c1f2
timcrowe91 <tim_crowe@hotmail.com>
2020-12-04 17:16:34 +0000

    new plot

---
+++ assignment-4b/make-activity/letters.R
@@ -0,0 +1,18 @@
+words <- readLines("words.txt")
+total_words <- length(words)
+
+letters_df <- data.frame(letters)
+letters_df <- cbind(proportion = 0, letters_df)
+
+for (i in 1:26){
+  
+  letters_df$proportion[i] = sum(grepl(letters_df$letters[i], words)) / total_words
+  
+}
+
+letters_plot <- ggplot(data = letters_df, aes(x = letters, y = proportion)) +
+  geom_bar(stat = "identity") +
+  labs(x = "Letter", y = "% of Words") +
+  theme_bw()
+
+ggsave("letters.png", letters_plot)
